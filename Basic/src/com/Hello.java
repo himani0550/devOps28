3515ba364fcac737ccf6b3165f99b4f2b44fc059
himanisi <himanisi@LIN51010822.corp.capgemini.com>
2021-01-28 12:50:12 +0530

    first message

---
+++ Basic/src/com/Hello.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello r u there?");
+		System.out.println("hey");
+
+	}
+
+}
---
+++ Basic/src/com/Input.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.util.Scanner;
+
+public class Input {
+	public static void main(String[] args)
+	{
+		int num1,num2,sum;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the first no.");
+		num1=sc.nextInt();
+		System.out.println("enter the second no.");
+		num2=sc.nextInt();
+		sum=num1+num2;
+		System.out.println("sum of the no. is=" +sum);
+	}
+	
+	
+	
+	}
+	
+
+
